1bc2b55b658f5aa34d0556bc87919c43223e7e15
Maxim Rodionov <rodionov.maxim05@gmail.com>
2025-11-09 19:39:58 +0300

    feat: task8 implementation

--- tests/autotests/test_task08.py
+++ tests/autotests/test_task08.py
@@ -17,7 +17,7 @@ from cfpq_concrete_cases import CASES_CFPQ, CaseCFPQ
 try:
     from project.cfpq_hellings import hellings_based_cfpq
     from project.cfpq_matrix import matrix_based_cfpq
-    from project.task8 import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
+    from project.cfpq_tensor import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
 except ImportError:
     pytestmark = pytest.mark.skip("Task 8 is not ready to test!")
 
